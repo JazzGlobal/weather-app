6cd4c4ad2cbe0ae7b5f4c7d4bc3c08a3d0ecc808
JazzGlobal <waymer147@gmail.com>
2019-03-21 12:39:21 -0400

    base line

--- weather-app/api/api.js
+++ weather-app/api/api.js
@@ -1,12 +1,20 @@
-var request = require('request');
+var request = require('request'),
+    api_key = '88910679cdcbe6ce47c23ad3de9a008b';
 
 const api =
 {
     getWeather(geolocation, callback){
-      request('', function(error, response, body){
+      var url = `https://api.openweathermap.org/data/2.5/forecast?lat=${geolocation.lat}&lon=${geolocation.lon}&appid=${api_key}`;
+      console.log('Requesting: ' + url)
+      request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
+          console.log('success');
           var data = JSON.parse(body);
-          callback(data);
+          callback(data, true);
+        }
+        else{
+          console.log(error);
+          console.log('Failed');
         }
       });
     }
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,17 @@
 var express = require('express'),
+    api = require('./api/api.js'),
+    geolocation = require('geolocation'),
+    bodyParser = require('body-parser'),
     app = express();
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Routes
-
-app.get('/', function(rreq, res){
-  res.send('index');
-})
+app.get('/', function(req, res){
+  res.render('index');
+});
 
 var port = process.env.port || 3000;
 app.listen(port, process.env.IP, function(){
