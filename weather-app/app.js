4040f5c85044abf3bbad8e710aeab994024262d4
JazzGlobal <waymer147@gmail.com>
2019-03-21 14:22:36 -0400

    Addednew route.

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,6 +12,13 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
+app.get('/weather', function(req, res){
+  var zipcode = req.query.zipcode;
+  api.getWeatherByZip(zipcode, function(data){
+    res.send(data);
+  });
+});
+
 var port = process.env.port || 3000;
 app.listen(port, process.env.IP, function(){
   console.log("Listening on : " + port);
