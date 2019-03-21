3b854c521b19909a6428d9a337801166b08fde73
JazzGlobal <waymer147@gmail.com>
2019-03-21 13:15:59 -0400

    Fixed public directory

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,7 +5,7 @@ var express = require('express'),
     app = express();
 
 app.set('view engine', 'ejs');
-app.use('/static', express.static('public'))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Routes
