55f814bfb2ff4ec5b0b67976d85142a2334e55d8
JazzGlobal <waymer147@gmail.com>
2019-03-22 20:51:00 -0400

    finished /weather route and passed weather data to weather.ejs

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,12 +14,23 @@ app.get('/', function(req, res){
 
 app.get('/weather', function(req, res){
   var zipcode = req.query.zipcode;
-  api.getWeatherByZip(zipcode, function(data){
-    res.send(data);
+  var weather =
+  {
+    current: '',
+    forecast: ''
+  }
+  //TODO: Add error handling in the event the first request fails. Send user to
+  api.getWeatherByZip(zipcode, function(current){
+    weather.current = current;
+    api.getForecastByZip(zipcode, function(forecast){
+      weather.forecast = forecast;
+      res.render('weather', {weatherData: weather});
+    });
   });
 });
 
 var port = process.env.port || 3000;
+
 app.listen(port, process.env.IP, function(){
   console.log("Listening on : " + port);
 });
